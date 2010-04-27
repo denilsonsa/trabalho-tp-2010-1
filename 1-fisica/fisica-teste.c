502527244b1d3861fda4b1bacbb060b752eab57d
Denilson Sá <denilsonsa@gmail.com>
2010-04-27 19:48:01 -0300

    Parâmetros são tratados num .c diferente. Camada física começa a sair do stub.

--- 1-fisica/fisica-teste.c
+++ 1-fisica/fisica-teste.c
@@ -1,101 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "fisica.h"
-
-
-struct program_arguments
-{
-	int local_port;
-	int remote_port;
-	char* remote_host;
-};
-
-
-// Global var used in print_help()
-char* g_program_name;
-
-
-void print_help()
-{
-	printf(
-		"Possible command-lines:\n"
-		"  %s <remote host>:<port>\n"
-		"  %s <local port> <remote host>:<port>\n"
-		"If the remote host is empty, it is a shortcut for the loopback address:\n"
-		"  %s <local port> :<port>\n",
-		g_program_name,
-		g_program_name,
-		g_program_name 
-	);
-}
-
-
-int split_host_port(char* input, char** host, int* port)
-{
-	// This function make *host point to a newly allocated string.
-	// Returns 1 on success, 0 on failure.
-
-	char* colon_pointer;
-
-	colon_pointer = strchr(input, ':');
-	if( ! colon_pointer )
-		return 0;
-
-	*host = strndup(input, colon_pointer - input);
-	*port = atoi(colon_pointer + 1);
-	return 1;
-
-	// GNU sscanf has 'a' modifier that allocates a string of the
-	// required size, but I chose to not use this feature.
-}
-
-
-void parse_arguments(int argc, char* argv[], struct program_arguments* args)
-{
-	int error = 0;
-
-	if( argc == 2 )
-	{
-		// progname host:port
-		args->local_port = 0;
-		if(! split_host_port( argv[1], &(args->remote_host), &(args->remote_port) ) )
-			error = 1;
-		if( args->remote_port == 0 )
-			error = 1;
-	}
-	else if( argc == 3 )
-	{
-		// progname localport host:port
-		args->local_port = atoi(argv[1]);
-		if(! split_host_port( argv[2], &(args->remote_host), &(args->remote_port) ) )
-			error = 1;
-		if( args->local_port == 0 )
-			error = 1;
-		if( args->remote_port == 0 )
-			error = 1;
-	}
-	else
-	{
-		print_help();
-		exit(1);
-	}
-	if( error == 1 )
-	{
-		printf("Error parsing parameters. (pass no parameters for help)\n");
-		exit(1);
-	}
-}
-
+#include "fisica-teste-args.h"
 
 int main(int argc, char* argv[])
 {
 	struct program_arguments args;
 
 	g_program_name = argv[0];
-
 	parse_arguments(argc, argv, &args);
+
 	printf("local_port: %d\n", args.local_port);
 	printf("remote_host: %s\n", args.remote_host);
 	printf("remote_port: %d\n", args.remote_port);
--- 1-fisica/fisica.c
+++ 1-fisica/fisica.c
@@ -1,5 +1,6 @@
 #include "fisica.h"
 
+#include <string.h>
 #include <malloc.h>
 
 #include <netinet/in.h>
@@ -10,9 +11,14 @@
 // };
 // struct sockaddr_in
 // {
-//     // other stuff here
+//     // __SOCKADDR_COMMON(sin_);, which is defined at <bits/sockaddr.h>,
+//     // which expands to: 
+//     sa_family_t sin_family;
+//     // which should have AF_INET value, defined at <bits/socket.h>
+//
 //     in_port_t sin_port;
 //     struct in_addr sin_addr;
+//
 //     // zero-padding here
 // };
 // #define INADDR_ANY       ((in_addr_t) 0x00000000)
@@ -23,6 +29,10 @@
 #include <sys/socket.h>
 // bind()
 
+#include <arpa/inet.h>
+// inet_aton()
+// inet_addr()
+
 
 // Global variables (for current state)
 static char* remote_host = NULL;
@@ -32,7 +42,7 @@ static struct sockaddr_in local_addr;
 static struct sockaddr_in remote_addr;
 
 
-void set_pointer(char ** cp, char * new_pointer)
+void set_pointer(void** cp, void* new_pointer)
 {
 	// This function frees the (*cp), if it is not null, and then make
 	// it point to the new_pointer
@@ -42,6 +52,34 @@ void set_pointer(char ** cp, char * new_pointer)
 	if( *cp )
 		free(*cp);
 	*cp = new_pointer;
+}
+
+
+int fill_remote_addr_struct()
+{
+	// Fills the remote_addr global var using the values of the other
+	// global vars.
+	//
+	// Returns 1 in success, or 0 in failure.
+
+	// If the remote host is empty, let's assume it's the loopback
+	if( remote_host[0] == '\0' )
+		remote_addr.sin_addr.s_addr = INADDR_LOOPBACK;
+	else
+	{
+		if( ! inet_aton(remote_host, &remote_addr.sin_addr) )
+		{
+			// Invalid address
+			return 0;
+		}
+	}
+	remote_addr.sin_port = htons(remote_port);
+	remote_addr.sin_family = AF_INET;
+
+	return 1;
+
+	// getaddrinfo() is powerful, but too complicated for this program.
+	// inet_aton() has error-checking, while inet_addr() doesn't.
 }
 
 
@@ -57,13 +95,25 @@ void Pex_Set_Local_Port(int port)
 
 int P_Activate_Request(int port, char* host)
 {
-	//set_pointer()
-	// See getaddrinfo(), inet_aton() or inet_addr()
+	// Returns 1 in success, or 0 in failure.
+	//
+	// The "host" string is duplicated here, so the caller is free to
+	// modify and free it. This internal copy is automatically freed
+	// when needed.
+
+	set_pointer((void*) &remote_host, strdup(host));
+	remote_port = port;
+
+	if( ! fill_remote_addr_struct() )
+		return 0;
+
+	return 1;
 	// TODO: stub
 }
 
 void P_Deactivate_Request(void)
 {
+	set_pointer((void*) &remote_port, NULL);
 	// TODO: stub
 }
 
