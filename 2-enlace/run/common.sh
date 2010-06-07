26244d29651e7bfdf42ba18222a5d3b4297826c4
Denilson Sá <denilsonsa@gmail.com>
2010-06-07 14:51:23 -0300

    README atualizado.

--- 2-enlace/run/common.sh
+++ 2-enlace/run/common.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+TERMINAL=xterm
+
 run() {
 	echo "$@"
-	xterm -geometry 50x10 -bg black -bd white -fg white -title "$*" -e "$@" &
+
+	# XXX: Edit the terminal command here!
+	$TERMINAL -geometry 50x10 -bg black -bd white -fg white -title "$*" -e "$@" &
+
+	# For xterm:
 	# -bg = background color
 	# -bd = border color
 	# -fg = foreground color
@@ -12,3 +18,18 @@ finish() {
 	echo "Press Ctrl+C here to close all windows opened by this script."
 	wait
 }
+
+
+# Some sanity checks below:
+
+if ! which $TERMINAL &>/dev/null ; then
+	echo "ERROR: '$TERMINAL' was not found. Please either install it"
+	echo "or edit 'run/common.sh' to run another terminal instead."
+	exit 1
+fi
+
+if [ -z "$DISPLAY" ] ; then
+	echo "ERROR: \$DISPLAY var is unset. You won't be able to launch"
+	echo "X11 terminal windows. (are you running this over ssh?)"
+	exit 1
+fi
